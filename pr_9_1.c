c992902c831f9fe97b36da9f672e6941a4f5b85b
Tanish-Sarkheliya <tanishsarkheliya4114@gmail.com>
2024-07-15 16:44:01 +0530

    project3

---
+++ pr_9_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char c='a';
+    do
+    {
+        printf("%c",c);
+        c+=4;
+
+    }while(c<'z');
+}
---
+++ pr_9_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n, ld, fd, sum=0;
+    printf("Enter N: ");
+    scanf("%d", &n);
+    do(n!=0);
+    {
+        ld=n%10;
+        fd=
+        sum=ld+fd;                                           
+    }while(n==0);
+    printf("Sum of last two digits:%d", sum);
+}
